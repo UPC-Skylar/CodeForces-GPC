f30b50f9b7fa648dbced3c4903ef6d7aa0549cda
Jack Tello <149704017+UPC-Skylar@users.noreply.github.com>
2024-12-20 09:44:50 -0500

    Add files via upload

---
+++ MaximumGCD/MaximunGCD.cpp
@@ -0,0 +1,13 @@
+// Source: https://usaco.guide/general/io
+
+#include <bits/stdc++.h>
+#include <numeric>
+using namespace std;
+
+int main() {
+	int t; cin >> t;
+	while(t--){
+		float x; cin >> x;
+		cout << floor(x / 2) << endl;
+	}
+}
