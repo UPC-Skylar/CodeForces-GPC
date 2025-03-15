ea0e8b3843c80b322cc6a2377c317b4d8e4251d2
Jack Tello <149704017+UPC-Skylar@users.noreply.github.com>
2025-03-15 18:24:08 -0500

    Add files via upload

---
+++ MagicalSticks/MagicalSticks.cpp
@@ -0,0 +1,15 @@
+// Source: https://usaco.guide/general/io
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve(){
+    ll x; cin >> x;
+    cout << (x + 1) / 2 << endl;
+}
+
+int main() {
+	int t; cin >> t;
+    while(t--) solve();
+}
